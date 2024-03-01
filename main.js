c6733b5c03c5728adb8bb6203d1bb72490a334a0
cypherHades <xjadezbix@gmx.com>
2024-03-01 09:06:13 -0500

    LinkedList dependency and modified auxiliary classes.
    I used the library "linked-list" because I did not trust my own implementation. However, this implementation wasn't easy to work with. It requires to create new `Item (Node)` objects and modify its `.value` every time you want to add a new node.
    
    The auxiliary classes `ModifiedSet` and `ModifiedMap` were necessary just because for ease of retrieval of keys. You can't use arrays as keys in a Map and also the `.has()` from Set. So I needed to parse the arrays as strings everytime I was about to retrieve, set, or check for some value. To avoid clogging the code with the parsing process in every part that I need to use `.get`, `.set` or `.has` methods, I made modified methods of these. In order to do this, It was needed to create a new class or modify the prototype of the orignal classes. I prefered the former.

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 function getAdjacents(coordinate) {
+import { List, Item } from "linked-list";
+class Board extends Map {
+	constructor() {
+		super();
+		this.mapBoard();
+	}
+class ModifiedSet extends Set {
+	hasCoordinate(coordinate) {
+		return this.has(`${coordinate}`);
+	}
+	addCoordinate(coordinate) {
+		this.add(`${coordinate}`);
+	}
+}
 	const [x, y] = coordinate;
 	const adjacents = [
 		[x - 2, y - 1],
